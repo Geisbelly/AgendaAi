88b5a702631ab3f4d7b0546d3f67d2f45424feea
Geisbelly <geisbelly19@gmail.com>
2025-03-07 11:29:36 -0300

    Remoções

--- src/app/Agenda.tsx
+++ src/app/Agenda.tsx
@@ -2,8 +2,6 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { Platform, View } from 'react-native';
 import 'react-native-reanimated';
-import { Tabs } from 'expo-router';
-import { Drawer } from 'expo-router/drawer'; // Importando a versão correta do Drawer para expo-router
 import { HapticTab } from '@/components/HapticTab';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Ionicons } from '@expo/vector-icons';
@@ -22,42 +20,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <>
       {/* Exibe Drawer para Web */}
       {Platform.OS === 'web' ? (
-        // <Drawer>
-        //   <Drawer.Screen name="Agenda" options={{ title: 'Agenda' }} initialParams={[WebAgenda]}/>
-        // </Drawer>
+
         <View><WebAgenda></WebAgenda></View>
       ) : (
-        // Exibe Tabs para dispositivos móveis
-        // <Tabs
-        //   screenOptions={{
-        //     headerShown: false,
-        //     tabBarButton: HapticTab,
-        //     tabBarBackground: TabBarBackground,
-        //     tabBarStyle: Platform.select({
-        //       ios: {
-        //         // Use a transparent background on iOS to show the blur effect
-        //         position: 'absolute',
-        //       },
-        //       default: {},
-        //     }),
-        //   }}
-        // >
-        //   <Tabs.Screen
-        //     name="Agenda"
-            
-        //     options={{
-        //       title: 'Agenda',
-        //       tabBarStyle: {boxShadow: '1px 0px 2px rgba(30, 127, 206, 0.39)', height: 58},
-        //       tabBarIcon: ({ color, focused }) => (
-        //         <Ionicons
-        //           name={focused ? "calendar" : "calendar-outline"}
-        //           size={30}
-        //           color={'rgba(21, 128, 235, 0.8)'}
-        //         />
-        //       ),
-        //     }}
-        //   />
-        // </Tabs>
+
        <View style={{ flex: 1 }}>
        <MobileAgenda></MobileAgenda>
         </View>
